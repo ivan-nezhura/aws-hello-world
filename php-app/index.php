a52a0e920a718ab1b989e8ce52f65a26c7d7718e
Ivan <ivan.nezhura@gmail.com>
2021-06-16 00:24:33 +0300

    first

---
+++ php-app/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Randomiser</title>
+</head>
+<body>
+<h1>Random number: <?= array_rand(range(0,100)) ?></h1>
+</body>
+</html>
